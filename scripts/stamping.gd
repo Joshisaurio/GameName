22542e4d56b25e00663ce5b00372ce2c144f2ad5
PlantCA <levesque.set@gmail.com>
2024-12-29 01:42:37 -0400

    WIP on Buncha-Stuff

--- scripts/stamping.gd
+++ scripts/stamping.gd
@@ -4,6 +4,10 @@ extends Node3D
 @onready var Dialogue = $UI/Fader/Dialogue
 
 var dialogue_speed := 0.03
+
+#Stamping stuff
+var total_evictions
+
 func _ready():
 	
 	UIAnim.play("IntroAnim")
@@ -18,5 +22,19 @@ func display_dialogue(text: String, speed: float):
 		Dialogue.text += char
 		await get_tree().create_timer(speed).timeout
 	
-	await get_tree().create_timer(5).timeout
+	await get_tree().create_timer(3).timeout
 	Dialogue.text = ""
+
+func generate_evictions():
+	
+	total_evictions = randi_range(8, 18) #How many papers the player must stamp, sign, etc
+	
+
+func show_next_eviction():
+	if total_evictions > 0:
+		show_eviction()
+		total_evictions -= 1
+	
+
+func show_eviction():
+	pass
