526d1190cc216ea9faca463679c986e1608007a9
Joshisaurio <joshmanjarrez13@gmail.com>
2024-12-29 13:12:03 -0600

    Added barebones buttons to pause
    Moved pause menu to a separate scene and added barebones buttons

--- scenes/pause.gd
+++ scenes/pause.gd
@@ -7,6 +7,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	visible = get_tree().paused
+	
 	if Input.is_action_just_released("pause"):
 		get_tree().paused = not get_tree().paused
+		
+	visible = get_tree().paused
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if get_tree().paused else Input.MOUSE_MODE_CAPTURED)
+		
+func _on_resume_pressed():
+	get_tree().paused = false
+
+
+func _on_back_to_menu_pressed():
+	get_tree().paused = false
+	get_tree().get_first_node_in_group("gamemanager").load_level(preload("res://scenes/title.tscn"))
